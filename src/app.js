70b81677c4052f8eab7ea0c7766a76fa0b266aac
Luc Dumas <lucpierrotdumas@gmail.com>
2022-02-08 19:36:35 -0500

    more CSS as well as an aADDENTRY post req

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const db = require('./db');
 const mongoose = require('mongoose');
+const { insert } = require('ramda');
 const Books = mongoose.model('books');
 
 // View Engine
@@ -20,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
 
-// What is the exact use of this middleware? I know it is something to do with a way to store user data between HTTP requests
+// What is the exact use of this middleware? I know it is something to do with a way to store user data between HTTP requests.. I believe this helps us access req.body
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
     secret: 'add session secret here!',
@@ -36,9 +37,9 @@ const test = "";
 app.get('/', (req, res) => {
     res.render('home', { 'test': test });
 })
-app.get('/addentry', (req, res) => {
-    res.render('addentry', { 'test': test });
-})
+// app.get('/addentry', (req, res) => {
+//     res.render('addentry', { 'test': test });
+// })
 app.get('/deleteentry', (req, res) => {
     res.render('deleteentry', { 'test': test });
 })
@@ -49,7 +50,72 @@ app.get('/register', (req, res) => {
     res.render('register', { 'test': test });
 })
 
+// Search
+// app.get('/addentry', (req, res) => {
+// })
 
+
+
+app.get('/addentry', (req, res) => {
+    let title = req.query.search;
+    if (title === "" || !title) {
+        Books.find(function (err, result) {
+            result.map(function callback(value, err) {
+                console.log(value.title);
+                console.log(err);
+
+            })
+            res.render('addentry', { 'result': result })
+            if (err) {
+                console.log("SOMETHING BAD HAPPENED")
+            }
+        })
+    } else {
+        Books.find({ "title": title }, function (err, result) {
+            result.map(function callback(value, err) {
+                console.log(value.title);
+                console.log(err);
+
+            })
+            res.render('addentry', { 'result': result })
+            if (err) {
+                console.log("SOMETHING BAD HAPPENED")
+            }
+        })
+    }
+})
+
+// Adding entries to the library
+app.post('/addentry', (req, res) => {
+    insert(req.body.title, req.body.author, req.body.genre, req.body.pages,
+        (error) => {
+            errorMsg = error.message;
+            res.render('addentry', {
+                'errorMsg': errorMsg
+            });
+        },
+        (success) => {
+            console.log(success);
+            console.log('success');
+            res.redirect('/addentry');
+        })
+    function insert(title, author, genre, pages, errorCallback, successCallback) {
+        const newBook = new Books({
+            title: title,
+            author: author,
+            genre: genre,
+            pages: pages
+        });
+        newBook.save(function (err, result) {
+            if (err) {
+                errorCallback({ message: "Please input a numerical value for the number of pages." });
+            } if (result) {
+                successCallback(result);
+            }
+        })
+
+    }
+})
 
 
 

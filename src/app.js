0fcf07f3a085740b297ac044487e6e0491399846
Luc Dumas <lucpierrotdumas@gmail.com>
2022-02-23 12:47:46 -0500

    first deployment

--- src/app.js
+++ src/app.js
@@ -180,8 +180,5 @@ app.post('/addentry', (req, res) => {
 })
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 8000;
-}
-app.listen(port);
+
+app.listen(process.env.PORT || 8000);
